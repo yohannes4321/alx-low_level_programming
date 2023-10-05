b82b0af4ce0c9a6d4233d3285a42830d8ae7a206
Your Name <alemuyohannes960@gmail.com>
2023-10-05 14:25:51 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description : 'to describe'
+ *
+ * return : Always 0 ( success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
