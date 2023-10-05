fc2a1d2bda444e6df0c40958bf28f96d2c9199bf
Your Name <alemuyohannes960@gmail.com>
2023-10-05 12:51:00 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - using puts function
+ * return: 0 (success)
+ */
+int main (void)
+{
+prints("\"Programming is like building a multilingual puzzl");
+return (0);
+}
