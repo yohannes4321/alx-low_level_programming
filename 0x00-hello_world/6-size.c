6fc621c3071f73e75f9c4d89f0cf053768c57fb9
Your Name <alemuyohannes960@gmail.com>
2023-10-05 16:00:38 -0700

    gkj

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(float);
-	return (0);
+	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(float));
+	return (0) ;
 }
