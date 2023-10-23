8e6f189a3c4ed68643002deac30d164a2ff4c775
Your Name <alemuyohannes960@gmail.com>
2023-10-23 04:42:33 -0700

    s

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
- * set_string - Entry point
- * @S: input
- * @to: inut 
- * Return : Always 0 (success)
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer we need to set to
+ * @to: string to set
  */
-void set-string(char **s,char *to)
+void set_string(char **s, char *to)
 {
 	*s = to;
 }
