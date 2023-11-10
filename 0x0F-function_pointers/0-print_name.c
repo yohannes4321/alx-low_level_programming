4d29813e6e6dd9306925a58880abdf39b0b0e532
Your Name <alemuyohannes960@gmail.com>
2023-11-10 05:35:02 -0800

    ee

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "function_pointers.h"
+/**
+ * main - print name using pointer funtuiion
+ * @f is the pointer funtion 
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+	if(name == NULL || f= NULL)
+		return;
+	f(name);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,4 @@
-#ifndef
-#define
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
 void print_name(char *name, void (*f)(char *));
 #endif
