6af5da53ff4efe554db37e781a1aa417be4b0df1
Your Name <alemuyohannes960@gmail.com>
2023-11-10 07:11:07 -0800

    ee

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * mian - function itrator
+ * size
+ * action is pinter function you goan use is the fubnction 
+ * return nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int size_t;
+	if (array == NULL ||action NULL)
+		return ;
+	for (int i=0; i< size_t; i++)
+	{
+		action(array[i]);
+	}
+
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
+#include <stdio.h>
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 #endif
