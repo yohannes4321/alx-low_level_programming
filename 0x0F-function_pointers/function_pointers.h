3d03df11fbf51248f9665fbf894646be9a5bef6a
Your Name <alemuyohannes960@gmail.com>
2023-11-10 07:14:33 -0800

    ee

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
-#include <stdio.h>
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 #endif
