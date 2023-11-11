189bbca2b3a0f78e107cc5977cd3132b5292ec3a
Your Name <alemuyohannes960@gmail.com>
2023-11-10 17:13:07 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,19 +1,22 @@
-#include "function_pointers"
+#include "function_pointers.h"
+
 /**
- * Prototype: int int_index(int *arr
+ * int_index - searches for an integer
+ * @array: array of type int
+ * @size: size of array
+ * @cmp: function with argument int and no return
+ *
+ * Return: No element matches -1, if size <=0 -1
+ * else index of first element cmp func does not rtrn 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if((array && size && cmp) >0)
-	{
-		for (int i=0 ;i< size ;i++)
-		{
-			if(cmp(array[i]))
-			return (i);
-		}
-	}
-	else 
-		return -1;
+	int i;
+
+	if (array && cmp && size > 0)
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+
+	return (-1);
 }
-
-
