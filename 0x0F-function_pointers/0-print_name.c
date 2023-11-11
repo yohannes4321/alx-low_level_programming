eca526aa9ff57c3fb1bee37b5cafdb525a6c9e85
Your Name <alemuyohannes960@gmail.com>
2023-11-10 18:38:31 -0800

    k

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
 #include "function_pointers.h"
+
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
+ * print_name - writes the character c to stdout
+ * @name: String
+ * @f: Pointer to function with char parameter and no return
  *
- * Return: Nothing.
+ * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if(name == NULL || char == NULL)
-		return;
-	f(name);
+	if (f != 0)
+		f(name);
 }
